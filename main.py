202ac388cb01c6d6fcd9b17c9623c39162a8b5ad
ZOUbohao <bzou@nvidia.com>
2023-02-24 14:28:23 +0800

    Update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 
 
-from .RUN_Functions import cleanMAGs
+from Deepurify.RUN_Functions import cleanMAGs
 
 
 if __name__ == "__main__":
-    input_mag_folder = "/path/to/your/mags/"
-    output_mag_folder = "/path/to/your/output/"
+    input_mag_folder = "../AllDataFFCCB/RealDataBinningModels/Concoct_Data/HLJ/fasta_bins/"
+    output_mag_folder = "..//AllDataFFCCB/RealDataBinningModels/Concoct_Data/HLJ/fasta_bins/DeepurifyBins/"
 
     cleanMAGs(
         input_bin_folder_path=input_mag_folder,
         output_bin_folder_path=output_mag_folder,
         bin_suffix="fa",
-        gpu_num=1,  # it can set 0 to use CPU, but it is much slower.
-        num_worker=2
+        gpu_num=2,  # it can set 0 to use CPU, but it is much slower.
+        num_worker=2,
+        batch_size_per_gpu=20
     )
