89d6ec6d8bcb57cccc1186b26a071a03f9f3d601
BohaoZou <806758760@qq.com>
2023-06-21 12:22:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
 from Deepurify.clean_func import cleanMAGs
 
 
-def read(file_path: str):
-    res = {}
-    with open(file_path, "r") as rh:
-        for line in rh:
-            info = line.strip("\n").split("\t")
-            idx = info[0].split("_")[0]
-            if idx not in res:
-                res[idx] = [tuple(info[0:-1])]
-            else:
-                res[idx].append(tuple(info[0:-1]))
-    return res
-
-
 if __name__ == "__main__":
-    
-    
-    # file1 = "/mnt/e/OriMetaInfo.txt"
-    # file2 = "/mnt/e/MetaInfo.txt"
-    
-    # res1 = read(file1)
-    # res2 = read(file2)
-    
-    # for key in res1.keys():
-    #     v1 = res1[key]
-    #     v2 = res2[key]
-    #     if len(v1) != len(v2):
-    #         print("##########")
-    #         print(v1)
-    #         print(v2)
-    
     inputBinFolderPath = "/root/Deepurify_Data/RealDataBinningModels/Concoct_Data/CAMI/high/fasta_bins/"
     outputBinFolderPath = "/root/Deepurify_Data/RealDataBinningModels/Concoct_Data/test_concoct_cami_high/"
     
